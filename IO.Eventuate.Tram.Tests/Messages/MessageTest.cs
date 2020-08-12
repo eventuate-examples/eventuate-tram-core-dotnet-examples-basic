3291b0e015e30cc5fb6b635021929b7b3ff4b6a1
Charanpreet <neeraj.kumari@techbitsolution.com>
2020-08-12 22:38:51 +0530

    pull request issues

--- IO.Eventuate.Tram.Tests/Messages/MessageTest.cs
+++ IO.Eventuate.Tram.Tests/Messages/MessageTest.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using Microsoft.EntityFrameworkCore;
+using IO.Eventuate.Tram.Messaging.Consumer;
+using System.Collections.Concurrent;
+using System.Threading.Channels;
 
 namespace IO.Eventuate.Tram.Tests
 {
@@ -24,71 +27,56 @@ namespace IO.Eventuate.Tram.Tests
         public string subscriberId = "";
         public string destination = "";
         public string payload = "";
-        public DatabaseMessageProducer messageProducer;
-        private List<IO.Eventuate.Tram.Messaging.Common.Message> queue = new List<IO.Eventuate.Tram.Messaging.Common.Message>();
+        IMessageProducer messageProducer;
+        IMessageConsumer messageConsumer;
+
+        private BlockingCollection<IMessage> queue = new BlockingCollection<IMessage>();
         public ISet<string> channels = new HashSet<string>();
+
         public MessageTest()
         {
             uniqueId = System.DateTime.Now.Ticks.ToString();
             subscriberId = "subscriberId" + uniqueId;
             destination = "destination" + uniqueId;
             payload = "Hello" + uniqueId;
+            channels.Add(destination);
         }
 
-        public class MessageProducer : DatabaseMessageProducer
+        [TestInitialize]
+        public void SetUp()
         {
-            public MessageProducer(IEnumerable<IMessageInterceptor> messageInterceptors, IIdGenerator idGenerator, IEventuateTramDbContextProvider dbContextProvider, ILogger<DatabaseMessageProducer> logger)
-                : base(messageInterceptors, idGenerator, dbContextProvider,
-                logger)
-            {
-
-            }
-        }
-        public class MessageConsumer : KafkaMessageConsumer
-        {
-            public MessageConsumer(string bootstrapServers,
-            EventuateKafkaConsumerConfigurationProperties eventuateKafkaConsumerConfigurationProperties,
-            DecoratedMessageHandlerFactory decoratedMessageHandlerFactory, ILoggerFactory loggerFactory,
-            IServiceScopeFactory serviceScopeFactory)
-                : base(bootstrapServers, eventuateKafkaConsumerConfigurationProperties, decoratedMessageHandlerFactory,
-                loggerFactory, serviceScopeFactory)
-            {
-
-            }
-        }
-        [TestMethod]
-        public void SendMessage()
-        {
+            //Producer
             IdGenerator generator = new IdGenerator();
             var options = new DbContextOptionsBuilder<EventuateTramDbContext>().UseSqlServer(TestSettings.EventuateTramDbConnection).Options;
             var schema = new EventuateSchema(TestSettings.EventuateTramDbSchema);
             EventuateTramDbContextProvider provider = new EventuateTramDbContextProvider(options, schema);
-            MessageProducer mp = new MessageProducer(new List<IMessageInterceptor>(),
-            generator, provider, Substitute.For<ILogger<DatabaseMessageProducer>>());
-            mp.Send(destination, MessageBuilder.WithPayload(payload).Build());
+            messageProducer = new DatabaseMessageProducer(new List<IMessageInterceptor>(), generator, provider, Substitute.For<ILogger<DatabaseMessageProducer>>());
 
-            Assert.IsNotNull(destination);
-        }
-
-        [TestMethod]
-        public void SubscribeMessage()
-        {
-            PrePostHandlerMessageHandlerDecorator obj = new PrePostHandlerMessageHandlerDecorator();
+            //Consumer
+            PrePostHandlerMessageHandlerDecorator prepostmessageHandlerDecorator = new PrePostHandlerMessageHandlerDecorator();
             IList<IMessageHandlerDecorator> decorators = new List<IMessageHandlerDecorator>();
-            decorators.Add((IMessageHandlerDecorator)obj);
-            var options = new DbContextOptionsBuilder<EventuateTramDbContext>().UseSqlServer(TestSettings.EventuateTramDbConnection).Options;
-
-            MessageConsumer mc = new MessageConsumer(TestSettings.KafkaBootstrapServers,
+            decorators.Add((IMessageHandlerDecorator)prepostmessageHandlerDecorator);
+            messageConsumer = new KafkaMessageConsumer(TestSettings.KafkaBootstrapServers,
                 EventuateKafkaConsumerConfigurationProperties.Empty(),
                 new DecoratedMessageHandlerFactory(decorators,
                 Substitute.For<ILogger<DecoratedMessageHandlerFactory>>()),
                 Substitute.For<ILoggerFactory>(),
                 Substitute.For<IServiceScopeFactory>());
-            channels.Add(subscriberId);
-            mc.Subscribe(subscriberId, channels, MessageHandler);
-            Assert.IsNotNull(subscriberId);
-        }
 
+        }
+        [TestMethod]
+        public void ShouldReceiveMessage()
+        {
+            messageConsumer.Subscribe(subscriberId, channels, MessageHandler);
+            messageProducer.Send(destination, MessageBuilder.WithPayload(payload).Build());
+
+            IMessage message;
+            queue.TryTake(out message, TimeSpan.FromSeconds(10));
+
+            Assert.IsNotNull(message);
+            Assert.Equals(payload, message.Payload);
+
+        }
 
         #region References
         public void MessageHandler(IMessage message, IServiceProvider serviceProvider)
--- IO.Eventuate.Tram.Tests/TestHelpers/TestSettings.cs
+++ IO.Eventuate.Tram.Tests/TestHelpers/TestSettings.cs
@@ -5,7 +5,7 @@
 	/// </summary>
 	public static class TestSettings
 	{
-		public static string KafkaBootstrapServers { get; set; } = "kafka:9092";
+		public static string KafkaBootstrapServers { get; set; } = "localhost:9092";
 		public static string EventuateTramDbConnection { get; set; } = "Server=LAPTOP-7G6A2VOR;Database=TramDb;User Id=sa;Password=Sa@123";
 		public static string EventuateTramDbSchema { get; set; } = "eventuate";
 	}
