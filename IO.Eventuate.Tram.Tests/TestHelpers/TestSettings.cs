5a39c2562e1006d850a84ad83b97d3b70336a2df
Charanpreet <charansingh83sh@gmail.com>
2020-08-14 00:05:53 +0530

    docker-compose.yml and test.sh added

--- IO.Eventuate.Tram.Tests/TestHelpers/TestSettings.cs
+++ IO.Eventuate.Tram.Tests/TestHelpers/TestSettings.cs
@@ -5,8 +5,8 @@
 	/// </summary>
 	public static class TestSettings
 	{
-		public static string KafkaBootstrapServers { get; set; } = "localhost:9092";
-		public static string EventuateTramDbConnection { get; set; } = "Server=LAPTOP-7G6A2VOR;Database=TramDb;User Id=sa;Password=Sa@123";
+		public static string KafkaBootstrapServers { get; set; } =  "kafka:9092";
+		public static string EventuateTramDbConnection { get; set; } = "Server=mssql,1433;Database=TramDb;User Id=sa;Password=App@Passw0rd";
 		public static string EventuateTramDbSchema { get; set; } = "eventuate";
 	}
 
---
+++ IO.Eventuate.Tram.Tests/test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+docker-compose down
+docker-compose up -d mssql
+
+# Wait for MSSQL to start up
+sleep 60s
+
+docker-compose run --rm dbsetup
+docker-compose up -d zookeeper
+docker-compose up -d kafka
+docker-compose up -d cdcservice
+                            
+# Wait for docker containers to start up
+sleep 60s
+
+# Print docker status
+docker ps
+docker stats --no-stream --all
+
+# Run tests
+docker-compose run --rm eventuatetramtests
+
+# Tear down test environment
+docker-compose down -v --remove-orphans
